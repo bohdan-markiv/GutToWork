bacce580ef11a213c1d4e22457720a9e51082dd4
bohdan-markiv <bohdan.markiv18@gmail.com>
2025-02-18 19:19:37 +0100

    -try new method

--- gut-to-work/next.config.ts
+++ gut-to-work/next.config.ts
@@ -2,6 +2,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: "/GutToWork",
+  basePath: "/GutToWork",
   output: "export",      // Enables static export mode
   trailingSlash: true,   // Optional: ensures trailing slash in URLs
   // ...other configuration options if needed
